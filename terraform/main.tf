0b8342bc6f05e5cc5a3a8924e09384bc467c68b1
Pavlo Kurochka <kurochka@gmail.com>
2024-03-25 20:43:41 -0700

    terraform

---
+++ terraform/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.12.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file(var.credentials)
+  project     = var.project
+  region      = var.region
+}
+
+
+resource "google_storage_bucket" "preparer-bucket" {
+  name          = var.gcs_bucket_name
+  location      = var.location
+  force_destroy = true
+
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
+
+
+
+resource "google_bigquery_dataset" "preparer_dataset" {
+  dataset_id = var.bq_dataset_name
+  location   = var.location
+}
+
+# export GOOGLE_APPLICATION_CREDENTIALS=/home/pavlo/terrademo/keys/my-creds.json
